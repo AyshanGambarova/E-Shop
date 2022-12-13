89f604199f890af5be9bb5652a99a052a2b9efcf
AyshanGambarova <ayshanag@code.edu.az>
2022-12-13 11:39:04 +0400

    carousel added

--- src/views/Dashboard/views/Products/views/ProductInfo/index.vue
+++ src/views/Dashboard/views/Products/views/ProductInfo/index.vue
@@ -1,37 +1,247 @@
 <template>
   <div class="bg-white px-8 py-6">
     <div class="pt-6 border border-slate-300 rounded-md border-black">
-       <nav aria-label="Breadcrumb">
-        <ol role="list" class="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+      <nav aria-label="Breadcrumb">
+        <ol
+          role="list"
+          class="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8"
+        >
           <li>
             <div class="flex items-center">
-              <span class="mr-2 text-sm font-medium text-gray-900">{{ product.category }}</span>
-              <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-5 w-4 text-gray-300">
+              <span class="mr-2 text-sm font-medium text-gray-900">{{
+                product.category
+              }}</span>
+              <svg
+                width="16"
+                height="20"
+                viewBox="0 0 16 20"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                class="h-5 w-4 text-gray-300"
+              >
                 <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
               </svg>
             </div>
           </li>
-           <li>
+          <li>
             <div class="flex items-center">
-              <span class="mr-2 text-sm font-medium text-gray-900">{{ product.title }}</span>
-              <svg width="16" height="20" viewBox="0 0 16 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="h-5 w-4 text-gray-300">
+              <span class="mr-2 text-sm font-medium text-gray-900">{{
+                product.title
+              }}</span>
+              <svg
+                width="16"
+                height="20"
+                viewBox="0 0 16 20"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                class="h-5 w-4 text-gray-300"
+              >
                 <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
               </svg>
             </div>
           </li>
           <li class="text-sm">
-            <span aria-current="page" class="font-medium text-gray-500 hover:text-gray-600">{{ product.brand }}</span>
+            <span
+              aria-current="page"
+              class="font-medium text-gray-500 hover:text-gray-600"
+              >{{ product.brand }}</span
+            >
           </li>
         </ol>
-      </nav> 
+      </nav>
+      <div id="indicators-carousel" class="relative" data-carousel="static">
+        <!-- Carousel wrapper -->
+        <div>
+          <h1>Carousel here</h1>
+          <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+            <div
+              class="hidden duration-700 ease-in-out"
+              data-carousel-item="active"
+            >
+              <img
+                src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+
+            <div
+              v-for="(imageItem, imageIndex) in product.images"
+              :key="imageIndex"
+              class="hidden duration-700 ease-in-out"
+              data-carousel-item
+            >
+              {{ imageIndex }}
+              {{ imageItem }}
+              <img
+                :src="imageItem"
+                :alt="imageItem"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              />
+            </div>
+          </div>
+          <!-- Slider indicators -->
+        </div>
+
+        <br />
+
+        <br />
+
+        <div>
+          <h1>Carousel here</h1>
+          <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+            <!-- Item 1 -->
+            <div
+              class="hidden duration-700 ease-in-out"
+              data-carousel-item="active"
+            >
+              <img
+                src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+              <img
+                src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+            <!-- Item 3 -->
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+              <img
+                src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+            <!-- Item 4 -->
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+              <img
+                src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+            <!-- Item 5 -->
+            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+              <img
+                src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+                class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+          </div>
+          <!-- Slider indicators -->
+        </div>
+
+        <div
+          class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2"
+        >
+          <button
+            type="button"
+            class="w-3 h-3 rounded-full"
+            aria-current="true"
+            aria-label="Slide 1"
+            data-carousel-slide-to="0"
+          ></button>
+          <button
+            type="button"
+            class="w-3 h-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 2"
+            data-carousel-slide-to="1"
+          ></button>
+          <button
+            type="button"
+            class="w-3 h-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 3"
+            data-carousel-slide-to="2"
+          ></button>
+          <button
+            type="button"
+            class="w-3 h-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 4"
+            data-carousel-slide-to="3"
+          ></button>
+          <button
+            type="button"
+            class="w-3 h-3 rounded-full"
+            aria-current="false"
+            aria-label="Slide 5"
+            data-carousel-slide-to="4"
+          ></button>
+        </div>
+        <!-- Slider controls -->
+        <button
+          type="button"
+          class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-prev
+        >
+          <span
+            class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+          >
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M15 19l-7-7 7-7"
+              ></path>
+            </svg>
+            <span class="sr-only">Previous</span>
+          </span>
+        </button>
+        <button
+          type="button"
+          class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-next
+        >
+          <span
+            class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"
+          >
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 5l7 7-7 7"
+              ></path>
+            </svg>
+            <span class="sr-only">Next</span>
+          </span>
+        </button>
+      </div>
       <!-- Image gallery -->
-      <div class="mx-auto px-4 pt-10 pb-16 sm:px-6 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
+      <div
+        class="mx-auto px-4 pt-10 pb-16 sm:px-6 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4"
+      >
         <div
           v-for="image in product.images"
           :key="image"
           class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
         >
-            <img class="rounded-t-lg" :src="image" :alt="image" />
+          <img class="rounded-t-lg" :src="image" :alt="image" />
         </div>
       </div>
       <!-- Product info -->
@@ -50,7 +260,7 @@
         <div class="mt-4 lg:row-span-3 lg:mt-0">
           <h2 class="sr-only">Product information</h2>
           <p class="text-3xl tracking-tight text-gray-900">
-           $ {{ product.price }}
+            $ {{ product.price }}
           </p>
 
           <!-- Reviews -->
